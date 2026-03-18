e9d4611e8eca6e04cb345a40372ef5d223dc530a
Kingtero17 <dhgords17@gmail.com>
2026-03-18 19:10:37 -0400

    Mejorando la portabilidad, se agrego arte y se mejoraron fn

--- Interfaz.h
+++ Interfaz.h
@@ -1,6 +1,20 @@
-//Declarando funciones interfaz.
+#ifndef INTERFAZ_H
+#define INTERFAZ_H
+
+#include <stdio.h>
+
+// Colores ANSI
+#define COLOR_ROJO     "\x1b[31m"
+#define COLOR_VERDE    "\x1b[32m"
+#define COLOR_AMARILLO "\x1b[33m"
+#define CP_AZUL        "\x1b[34m"
+#define CP_CIAN        "\x1b[36m"
+#define COLOR_RESET    "\x1b[0m"
+
+// Declarando funciones interfaz.
 void gotoxy(int x, int y);
 void ocultar_cursor();
+void limpiar_pantalla();
 void pintar_marco();
 void menu_inicio();
 void menu_controles();
@@ -9,143 +23,179 @@ void menu_game_over();
 void menu_post_puntaje();
 void pantalla_final();
 
-//Definiendo funciones interfaz.
-
-//Funcion gotoxy; coordenadas.
+// Funcion gotoxy (ANSI)
 void gotoxy(int x, int y){
-	HANDLE hCon;
-	COORD dwPos;
-	dwPos.X = x;
-	dwPos.Y = y;
-	hCon = GetStdHandle( STD_OUTPUT_HANDLE);
-	SetConsoleCursorPosition(hCon,dwPos);
+    printf("\033[%d;%dH", y, x);
 }
 
-//Ocultar cursor; como su nombre lo indica oculta el cursor de la pantalla.
+// Ocultar cursor (ANSI)
 void ocultar_cursor(){
-	CONSOLE_CURSOR_INFO cci = {100, FALSE};
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cci);
+    printf("\033[?25l");
 }
 
-//Pintar marco; marco del juego.
-void pintar_marco(){
-	//Lineas horizontales.
-	for(int i = 2; i < 118; i++){
-		gotoxy(i, 2); printf("%c",205);
-		gotoxy(i, 28);printf("%c",205);
-	}
-	//Lineas verticales.
-	for(int i = 2; i < 29; i++){
-		gotoxy(2, i);  printf("%c",186);
-		gotoxy(117, i);printf("%c",186);
-	}
-    //Esquinas.
-    gotoxy(2,2);   printf("%c",201);
-    gotoxy(2,28);  printf("%c",200);
-    gotoxy(117,2); printf("%c",187);
-    gotoxy(117,28);printf("%c",188);
+// Limpiar pantalla (ANSI)
+void limpiar_pantalla(){
+    printf("\033[2J\033[H");
 }
 
-//Menu de inicio.
+#ifdef _WIN32
+    // FUNCIÓN PARA WINDOWS
+    void pintar_marco() {
+        // Lineas horizontales (Código 205: ═)
+        for(int i = 2; i < 118; i++) {
+            gotoxy(i, 2);  printf("%c", 205);
+            gotoxy(i, 28); printf("%c", 205);
+        }
+        // Lineas verticales (Código 186: ║)
+        for(int i = 2; i < 29; i++) {
+            gotoxy(2, i);   printf("%c", 186);
+            gotoxy(117, i); printf("%c", 186);
+        }
+        // Esquinas (201: ╔, 187: ╗, 200: ╚, 188: ╝)
+        gotoxy(2, 2);    printf("%c", 201);
+        gotoxy(117, 2);  printf("%c", 187);
+        gotoxy(2, 28);   printf("%c", 200);
+        gotoxy(117, 28); printf("%c", 188);
+    }
+#else
+    // FUNCIÓN PARA LINUX (Usa secuencias UTF-8)
+    void pintar_marco() {
+        for(int i = 2; i < 118; i++) {
+            gotoxy(i, 2);  printf("═");
+            gotoxy(i, 28); printf("═");
+        }
+        for(int i = 2; i < 29; i++) {
+            gotoxy(2, i);   printf("║");
+            gotoxy(117, i); printf("║");
+        }
+        gotoxy(2, 2);    printf("╔");
+        gotoxy(117, 2);  printf("╗");
+        gotoxy(2, 28);   printf("╚");
+        gotoxy(117, 28); printf("╝");
+    }
+#endif
+
+
 void menu_inicio(){
-	//Marco del Menu del juego.
+    limpiar_pantalla();
+    pintar_marco();
 
-	//Lineas horizontales.
-	for(int i = 2; i < 118; i++){
-		gotoxy(i, 2); printf("%c",205);
-		gotoxy(i, 28);printf("%c",205);
-	}
-	//Lineas Verticales.
-	for(int i = 2; i < 29; i++){
-		gotoxy(2, i);  printf("%c",186); 
-		gotoxy(117, i);printf("%c",186);
-	}
-    //Esquinas.
-    gotoxy(2,2);   printf("%c",201);
-    gotoxy(2,28);  printf("%c",200);
-    gotoxy(117,2); printf("%c",187);
-    gotoxy(117,28);printf("%c",188);
+    const char* portada[21] = {
+    "        aaaaaaaaaaaa  aa         aa      aaaa      aa       aa aaaaaaaaaaaa                                   ",
+    "                  aa  aaa        aa    aaaaaaaa    aa    aa    aa                                             ",
+    "                  aa  aa  aa     aa  aaaaaaaaaaaa  a  aa       aa                                             ",
+    "        aaaaaaaaaaaa  aa   aaa   aa aaaaaaaaaaaaaa aa          aaaaaaaaaaaa                                   ",
+    "        aa            aa     aa  aa  aaaaaaaaaaaa  a aa        aa                                             ",
+    "        aa            aa        aaa    aaaaaaaa    aa   aa     aa                                             ",
+    "        aaaaaaaaaaaa  aa         aa      aaaa      aa      aa  aaaaaaaaaaaa                                   ",
+    "                                     **        **                              -PRESIONE 1 PARA JUGAR.        ",
+    "                                     ****    ****             ************     -PRESIONE 2 PARA VER PUNTAJES  ",
+    "                                     ** ****** **             ************     -PRESIONE 3 PARA VER CONTROLES ",
+    "     ********************************************             ************     -PRESIONE ESC PARA SALIR.      ",
+    "     ********************************************             ************                                    ",
+    "     ********************************************             ************                                    ",
+    "     ********************************************             ************                                    ",
+    "     ************                                             ************                                    ",
+    "     ************            *********************************************                                    ",
+    "     ************            *********************************************                                    ",
+    "     ************            *********************************************                                    ",
+    "     *********************************************************************                                    ",
+    "     *************************************                                                                    ",
+    "     *************************************                                                                    ",
 
-//Portada de inicio del juego.
-char portada [22][111] =		// 112 - 2 = 110 en le eje "x" y 21 de en el eje "y".
-{
-"        aaaaaaaaaaaa  aa         aa      aaaa      aa       aa aaaaaaaaaaaa                                   ",
-"                  aa  aaa        aa    aaaaaaaa    aa    aa    aa                                             ",
-"                  aa  aa  aa     aa  aaaaaaaaaaaa  a  aa       aa                                             ",
-"        aaaaaaaaaaaa  aa   aaa   aa aaaaaaaaaaaaaa aa          aaaaaaaaaaaa                                   ",
-"        aa            aa     aa  aa  aaaaaaaaaaaa  a aa        aa                                             ",
-"        aa            aa        aaa    aaaaaaaa    aa   aa     aa                                             ",
-"        aaaaaaaaaaaa  aa         aa      aaaa      aa      aa  aaaaaaaaaaaa                                   ",
-"                                     **        **                              -PRESIONE 1 PARA JUGAR.        ",
-"                                     ****    ****             ************     -PRESIONE 2 PARA VER PUNTAJES  ",
-"                                     ** ****** **             ************     -PRESIONE 3 PARA VER CONTROLES ",
-"     ********************************************             ************     -PRESIONE ESC PARA SALIR.      ",
-"     ********************************************             ************                                    ",
-"     ********************************************             ************                                    ",
-"     ********************************************             ************                                    ",
-"     ************                                             ************                                    ",
-"     ************            *********************************************                                    ",
-"     ************            *********************************************                                    ",
-"     ************            *********************************************                                    ",
-"     *********************************************************************                                    ",
-"     *************************************                                                                    ",
-"     *************************************                                                                    ",
+    };
 
-};
-
-	for(int i = 0 ; i < 21; i++){
-		for(int j = 0; j < 109; j++){
-			gotoxy(j + 5, i + 5); printf("%c", portada[i][j]);
-		}
-	}
+    for(int i = 0 ; i < 21; i++){
+        gotoxy(5, i + 5); 
+        printf("%s", portada[i]);
+    }
 }
 
-//Menu controles.
-void menu_controles(){
-	system("cls");
-	pintar_marco();
-	gotoxy(53, 9); printf("CONTROLES:");
-	gotoxy(40, 11);printf("MOVER ARRIBA:     Flecha Arriba    %c", 30);
-	gotoxy(40, 12);printf("MOVER ABAJO:      Flecha Abajo     %c", 31);
-	gotoxy(40, 13);printf("MOVER DERECHA:    Flecha Derecha   %c", 16);
-	gotoxy(40, 14);printf("MOVER IZQUIERDA:  Flecha Izquierda %c", 17);
-	gotoxy(40, 15);printf("PAUSAR JUEGO:     Numero tres      %c", 51);
-	gotoxy(31, 17);printf("Presione cualquier tecla para volver al menu de inicio.");
+
+void menu_controles() {
+    limpiar_pantalla();
+    pintar_marco();
+
+    int a = 33;
+    gotoxy(a, 6);  printf(COLOR_AMARILLO "  ____ _____   _  ___ ______ _____    __    ____ ____ " COLOR_RESET);
+    gotoxy(a, 7);  printf(COLOR_AMARILLO " / __// __  \\ / \\/  //_  __/ / __/   / /   / __// __/ " COLOR_RESET);
+    gotoxy(a, 8);  printf(COLOR_AMARILLO "/ /_ / /_ / // \\/  /  / / / / / o \\ / /__ / _/_ \\ \\  " COLOR_RESET);
+    gotoxy(a, 9);  printf(COLOR_AMARILLO "\\___/\\_____//_/ \\_/  /_/ /_/\\_\\___//____/ \\___//___/ " COLOR_RESET);
+
+    // Controles
+    int b = 45;
+    gotoxy(b, 12); printf("ARRIBA:    [ W ]  o  [ ^ ]");
+    gotoxy(b, 13); printf("ABAJO:     [ S ]  o  [ v ]");
+    gotoxy(b, 14); printf("IZQUIERDA: [ A ]  o  [ < ]");
+    gotoxy(b, 15);      printf("DERECHA:   [ D ]  o  [ > ]");
+
+    gotoxy(b, 17); printf(COLOR_ROJO "PAUSA:     [ 3 ]" COLOR_RESET);
+    gotoxy(b, 18); printf(COLOR_ROJO "SALIR:     [ ESC ]" COLOR_RESET);
+
+    gotoxy(38, 22); printf("PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU...");
 }
 
-//Menu de pausa.
 void menu_pausa(){
-	gotoxy(56, 7); printf("PAUSE");
-	gotoxy(28, 9); printf("SALIR:              Presione ESC para salir.");
-	gotoxy(28, 10);printf("REINICIAR PARTIDA:  Presione 1 para reiniciar la partida.");
-	gotoxy(28, 11);printf("MENU DE INICIO:     Presione 2 para volver al menu de inicio.");
-	gotoxy(28, 12);printf("REANUDAR PARTIDA:   Presione 3 para reanudar la partida.");
+    gotoxy(56, 7); printf("PAUSE");
+    gotoxy(28, 9); printf("SALIR:              Presione ESC para salir.");
+    gotoxy(28, 10);printf("REINICIAR PARTIDA:  Presione 1 para reiniciar la partida.");
+    gotoxy(28, 11);printf("MENU DE INICIO:     Presione 2 para volver al menu de inicio.");
+    gotoxy(28, 12);printf("REANUDAR PARTIDA:   Presione 3 para reanudar la partida.");
 }
 
-//Menu Game over.
 void menu_game_over(){
-	gotoxy(54, 7); printf("GAME OVER");
-	gotoxy(28, 9); printf("SALIR:              Presione ESC para salir.");
-	gotoxy(28, 10);printf("REINICIAR PARTIDA:  Presione 1 para reiniciar la partida.");
-	gotoxy(28, 11);printf("GUARDAR RECORD:     Presione 2 para guardar su record.");
-    gotoxy(28, 12);printf("MENU DE INICIO:     Presione 3 para volver al menu de inicio.");
+    limpiar_pantalla();
+    pintar_marco();
+
+    int c = 32; 
+    gotoxy(c, 7);  printf(COLOR_ROJO " _________    __  _________   ____ _   ____________ " COLOR_RESET);
+    gotoxy(c, 8);  printf(COLOR_ROJO "/ ____/   |  /  |/  / ____/  / __ \\ | / / ____/ __ \\" COLOR_RESET);
+    gotoxy(c, 9);  printf(COLOR_ROJO "/ / __/ /| | / /|_/ / __/    / / / / |/ / __/ / /_/ /" COLOR_RESET);
+    gotoxy(c, 10); printf(COLOR_ROJO "/ /_/ / ___ |/ /  / / /___   / /_/ /|  / /___/ _, _/ " COLOR_RESET);
+    gotoxy(c, 11); printf(COLOR_ROJO "\\____/_/  |_/_/  /_/_____/   \\____/ |_/_____/_/ |_|  " COLOR_RESET);
+
+    int a = 28;
+    gotoxy(a, 13);printf("SALIR:              Presione ESC para salir.");
+    gotoxy(a, 14);printf("REINICIAR PARTIDA:  Presione 1 para reiniciar la partida.");
+    gotoxy(a, 15);printf("GUARDAR RECORD:     Presione 2 para guardar su record.");
+    gotoxy(a, 16);printf("MENU DE INICIO:     Presione 3 para volver al menu de inicio.");
 }
 
-//Menu post_puntaje.
 void menu_post_puntaje(){
-	system("cls");
-	pintar_marco();
-	gotoxy(42, 7); printf("ELIGE UNA OPCION PARA CONTINUAR:");
-	gotoxy(28, 9); printf("SALIR:              Presione ESC para salir.");
-	gotoxy(28, 10);printf("JUGAR DE NUEVO:     Presione 1 para jugar de nuevo.");
+    limpiar_pantalla();
+    pintar_marco();
+    gotoxy(42, 7); printf("ELIGE UNA OPCION PARA CONTINUAR:");
+    gotoxy(28, 9); printf("SALIR:              Presione ESC para salir.");
+    gotoxy(28, 10);printf("JUGAR DE NUEVO:     Presione 1 para jugar de nuevo.");
     gotoxy(28, 11);printf("MENU DE INICIO:     Presione 2 para volver al menu de inicio.");
 }
 
-//Pantalla final del juego.
 void pantalla_final(){
-	system("cls");
+    limpiar_pantalla();
     pintar_marco();
-    gotoxy(49,13);printf("Gracias por jugar!");
-    gotoxy(32,14);printf("Presione cualquier tecla para finalizar el programa.");
-	system("pause > NULL");   //System("pause") nos funciona para que el programa se pause y no se cierre solo.
+
+int x_centro = 30; 
+    int y_ini = 8;
+
+    // Se duplicaron las barras invertidas (\\) para que el compilador no de error
+    gotoxy(x_centro, y_ini++); printf(COLOR_VERDE "  _____ _   _    _   _   _ _  __ ____    _____ ___  ____  " COLOR_RESET);
+    gotoxy(x_centro, y_ini++); printf(COLOR_VERDE " |_   _| | | |  / \\ | \\ |  ||/ // ___|  |  ___/ _ \\|  _ \\ " COLOR_RESET);
+    gotoxy(x_centro, y_ini++); printf(COLOR_VERDE "   | | | |_| | / _ \\|  \\|  |' / \\___ \\  | |_ | | | | |_) |" COLOR_RESET);
+    gotoxy(x_centro, y_ini++); printf(COLOR_VERDE "   | | |  _   / ___ \\  |\\  |.  \\___) |  |  _|| |_| |  _ < " COLOR_RESET);
+    gotoxy(x_centro, y_ini++); printf(COLOR_VERDE "   |_| |_| |_/_/   \\_\\_| \\_|_|\\_\\____/  |_|   \\___/|_| \\_\\" COLOR_RESET);
+    
+    // Segunda parte: PLAYING
+    gotoxy(x_centro+8, y_ini++); printf(COLOR_AMARILLO "  ____  _        _ __   _____ _   _  ____ " COLOR_RESET);
+    gotoxy(x_centro+8, y_ini++); printf(COLOR_AMARILLO " |  _ \\| |      / \\\\ \\ / /_ _| \\ | |/ ___|" COLOR_RESET);
+    gotoxy(x_centro+8, y_ini++); printf(COLOR_AMARILLO " | |_) | |     / _ \\\\ V / | ||  \\| | |  _ " COLOR_RESET);
+    gotoxy(x_centro+8, y_ini++); printf(COLOR_AMARILLO " |  __/| |___ / ___ \\| |  | || |\\  | |_| |" COLOR_RESET);
+    gotoxy(x_centro+8, y_ini++); printf(COLOR_AMARILLO " |_|   |_____/_/   \\_\\_| |___|_| \\_|\\____|" COLOR_RESET);
+
+    gotoxy(50,19);printf("GRACIAS POR JUGAR!");
+    gotoxy(39,20);printf("PRESIONE ENTER PARA FINALIZAR EL PROGRAMA.");
+
+    while (getchar() != '\n' && getchar() != EOF);
+    getchar(); 
 }
+
+#endif // INTERFAZ_H
